7425d5e4ab61d6f46b10ad351014bea935dcd7f2
MatthewFoster02 <mattfoster02@outlook.com>
2023-06-15 00:45:53 +0200

    Initialised Shapley value class

---
+++ cooperative_functions/shapley_value.py
@@ -0,0 +1,31 @@
+import itertools
+
+class ShapleyValue:
+    def __init__(self, coalitions:dict=None, num_players:int=None):
+        self.coalitions = coalitions
+        self.num_players = num_players
+
+    def get_shapley_value(self):
+        # Create all orderings
+        # For each ordering, generate a vector with value for each player
+        # Average the values given to each player to get Shapley value
+
+        orderings = self.get_all_orderings()
+
+        values = {}
+        for order in orderings:
+            values[order] = self.get_values(order)
+        
+        shapley_value = self.average_values(values)
+        return shapley_value
+    
+    def get_all_orderings(self):
+        numbers = ''.join(str(i) for i in range(1, self.num_players + 1))
+        orderings = [''.join(p) for p in itertools.permutations(numbers)]
+        return orderings
+
+    def get_values(self, order:str):
+        pass
+
+    def average_values(self, values:dict):
+        pass
---
+++ testing/test_shapley_value.py
@@ -0,0 +1,4 @@
+from cooperative_functions.shapley_value import ShapleyValue
+
+def test_get_all_orderings():
+    pass
